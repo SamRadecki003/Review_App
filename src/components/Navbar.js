268bd89a7a4f8559e9d8830629b3921d3f14cf9a
Samantha Radecki <samantha.radecki@gmail.com>
2021-09-29 12:12:14 -0500

    NavBar complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-ui/core";
+
+import PetsIcon from "@material-ui/icons/Pets";
+import HomeIcon from "@material-ui/icons/Home";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <PetsIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Sam's Review App
+          </Typography>
+          <Button color="inherit">
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <HomeIcon />
+            </IconButton>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
