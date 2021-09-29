ebeb8f2084b6d18d249a9487bbbab446ba048643
Samantha Radecki <samantha.radecki@gmail.com>
2021-09-29 16:49:09 -0500

    single review page up

--- src/components/IndividualReview.js
+++ src/components/IndividualReview.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import reviews from "../reviews";
+import StarRatings from "react-star-ratings";
+import { Typography, Card, CardContent } from "@material-ui/core";
 
-export default function IndividualReview() {
+export default function IndividualReview(props) {
+  const individualReviewId = props.match.params.reviewId;
+  let review = "";
+  for (let i = 0; i < reviews.length; i++) {
+    if (reviews[i].id === individualReviewId) {
+      review = reviews[i];
+      console.log("set state!");
+    }
+  }
+  const [singleReview] = useState(review);
+  const date = singleReview.published_at.slice(0, 14);
+  console.log(singleReview);
   return (
-    <div>
-      <h1>this is a single review</h1>
-    </div>
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography gutterBottom>{singleReview.place}</Typography>
+        <StarRatings
+          rating={singleReview.rating}
+          starRatedColor="#FFD700"
+          numberOfStars={5}
+          starDimension="30px"
+          starSpacing="5px"
+          name="rating"
+        />
+        <Typography variant="body2">{singleReview.content}</Typography>
+      </CardContent>
+      <CardContent>
+        <Typography sx={{ mb: 1.5 }} gutterBottom>
+          {singleReview.author}
+        </Typography>
+        <Typography align="right" gutterBottom>
+          {date}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
-// import history from "../history";
 import PetsIcon from "@material-ui/icons/Pets";
 import HomeIcon from "@material-ui/icons/Home";
 
